1b0bf43983e1b572100c28dbc32f2ef665074b31
Max Heckel <maxheckel@Maxs-MacBook-Pro.local>
2019-08-01 11:18:06 -0400

    Collection refresh

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -16,6 +16,9 @@
             <a @click="persist" class="item">
                 Persist Albums
             </a>
+            <a @click="refresh" class="item">
+                Refresh Albums <font-awesome-icon v-bind:class="{'spinning-vinyl': true, 'spinning': isSpinning}" :icon="['fal', 'compact-disc']" />
+            </a>
             <div @click="logout" class="item last-item">
                 Log Out
             </div>
@@ -25,12 +28,15 @@
 </template>
 
 <script>
+
+    import api from './../apis/discogs'
     export default {
         name: "custom-dropdown",
         data: function(){
             return {
                 showing: false,
-                persisted: null
+                persisted: null,
+                isSpinning: false
             }
         },
         computed: {
@@ -41,7 +47,34 @@
                 return this.persisted ? this.persisted : (localStorage.getItem('last-persisted') ? localStorage.getItem('last-persisted') : 'Never')
             }
         },
+        mounted(){
+            document.body.addEventListener('click', (e) => {
+                if(!e.target.matches('.dropdown *')){
+                    this.showing = false
+                }
+            })
+        },
         methods: {
+            async refresh(){
+                let page = 1;
+
+                this.isSpinning = true;
+                let collection = await api.getCollection(this.$store.state.discogs.discogsUsername, this.$store.state.discogs.discogsOauthToken, this.$store.state.discogs.discogsOauthSecret, page)
+                var releases = collection.data.releases;
+                while(collection.data.releases !== undefined && collection.data.pagination.page < collection.data.pagination.pages){
+                    page = page + 1;
+                    try{
+                        collection = await api.getCollection(this.$store.state.discogs.discogsUsername, this.$store.state.discogs.discogsOauthToken, this.$store.state.discogs.discogsOauthSecret, page)
+                    } catch {
+                        break;
+                    }
+                    releases.push(...collection.data.releases)
+                }
+
+                this.$store.commit('discogs/setDiscogsCollection', {collection: releases})
+                this.isSpinning = false
+                alert('Collection refreshed, found '+releases.length+' albums.')
+            },
             persist: function(){
                 localStorage.setItem('discogs-data', JSON.stringify(this.$store.state.discogs.collection))
                 var today  = new Date();
